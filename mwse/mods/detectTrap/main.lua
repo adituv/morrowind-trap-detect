e05bd1c00fb9c08860791135d5a90687d1233049
Iris Ward <aditu.venyhandottir@gmail.com>
2019-08-29 11:39:12 +0100

    Draft add light to containers
    Attempt to add light to containers upon pressing K.
    Crashes Morrowind during the call to getOrCreateAttachedDynamicLight

--- mwse/mods/detectTrap/main.lua
+++ mwse/mods/detectTrap/main.lua
@@ -5,3 +5,36 @@ event.register("modConfigReady", function()
 end);
 
 detectTrap:init();
+
+local highlightTraps = function()
+  local cell = tes3.mobilePlayer.cell;
+  
+  for v in cell:iterateReferences(tes3.objectType.container) do
+    local oldlight = v:getAttachedDynamicLight();
+    if oldLight ~= nil then
+      v:detachDynamicLightFromAffectedNodes()
+    end
+    
+    local light = {
+      constantAttenuation = 0.0,
+      linearAttenuation = 0.0,
+      quadraticAttenuation = 3.5,
+      ambient = {
+        r = 1.0,
+        g = 0.0,
+        b = 0.0
+      }
+    };
+    
+    
+    local lightNode = {
+      light = light,
+      value = 1.0
+    };
+    
+    v:getOrCreateAttachedDynamicLight(lightNode);
+  end
+end;
+
+
+event.register("keyDown", highlightTraps, { filter = tes3.scanCode.k });
